0064beb36a1d745a0758b28663e92de09bdb18e9
qinbinyuan <binyuan_qin@163.com>
2018-12-29 21:05:02 +0800

    完成商家实景图片展示功能

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="seller">
+  <div class="seller" ref="seller">
     <div class="seller-content">
       <div class="overview">
         <h1 class="title">{{seller.name}}</h1>
@@ -12,7 +12,7 @@
           <li class="block">
             <h2>起送价</h2>
             <div class="content">
-              <span class="stress">{{seller.minPrice}}</span>
+              <span class="stress">{{seller.minPrice}}</span>元
             </div>
           </li>
           <li class="block">
@@ -27,20 +27,96 @@
           </li>
         </ul>
       </div>
+      <v-split></v-split>
+      <div class="bulletin">
+        <h1 class="title">公告与活动</h1>
+        <div class="content-wrapper border-1px">
+          <p class="content">{{seller.bulletin}}</p>
+        </div>
+        <ul v-if="seller.supports" class="supports">
+          <li class="support-item border-1px" v-for="(item,key) in seller.supports" :key="key">
+              <span class="icon"
+                    :class="classMap[seller.supports[key].type]"></span>
+            <span class="text">{{seller.supports[key].description}}</span>
+          </li>
+        </ul>
+        <!--<div class="favorite" @click="toggleFavorite">
+          <span class="icon-favorite" :class="{active:favorite}"></span>
+          <span class="text">{{favoriteText}}</span>
+        </div>-->
+      </div>
+      <v-split></v-split>
+      <div class="pics">
+        <h1 class="title">商家实景</h1>
+        <div class="pic-wrapper" ref="picwrapper">
+          <ul class="pic-list" ref="piclist">
+            <li :key="index" class="pic-item" v-for="(pic,index) in seller.pics">
+              <img :src="pic" width="120" height="90" alt="商家实景">
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import star from '../star/star';
+  import split from '../split/split';
+  import BScroll from 'better-scroll';
   export default {
     props: {
       seller: {
         type: Object
       }
     },
+    created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+    },
+    watch: {
+      'seller'() {
+        this.$nextTick(() => {
+          this._initScroll();
+          this._initPicscroll();
+        });
+      }
+    },
+    methods: {
+      _initScroll() {
+        if (!this.scroll) {
+          this.scroll = new BScroll(this.$refs.seller, {
+            click: true
+          });
+        } else {
+          this.scroll.refresh();
+        }
+      },
+      _initPicscroll() {
+        if (this.seller.pics) {
+          let picWidth = 120;
+          let margin = 6;
+          let width = (picWidth + margin) * this.seller.pics.length - margin;
+          this.$refs.piclist.style.width = width + 'px';
+          this.$nextTick(() => {
+            if (!this.picScroll) {
+              this.picScroll = new BScroll(this.$refs.picwrapper, {
+                scrollX: true,
+                eventPassthrough: 'vertical'
+              });
+            } else {
+              this.picScroll.refresh();
+            }
+          });
+        }
+      }
+    },
+    mounted() {
+      this._initScroll();
+      this._initPicscroll();
+    },
     components: {
-      'v-star': star
+      'v-star': star,
+      'v-split': split
     }
   };
 </script>
@@ -97,4 +173,66 @@
             color: rgb(7,17,27)
             .stress
               font-size: 24px
+    .bulletin
+      padding:18px 18px 0 18px
+      .title
+        margin-bottom:8px
+        line-height:14px
+        color: rgb(7, 17, 27)
+      .content-wrapper
+        padding: 0 12px 16px 12px
+        border-1px(rgba(7,17,27,0.1))
+        .content
+          line-height: 24px
+          font-size: 12px
+          color: rgb(240,20,20)
+      .supports
+        .support-item
+          padding: 16px 12px
+          border-1px(rgba(7,17,27,0.1))
+          font-size: 0
+          &:last-child
+            border-none()
+          .icon
+            display: inline-block
+            width: 16px
+            height: 16px
+            margin-right: 6px
+            vertical-align: top
+            background-size: 16px 16px
+            background-repeat: no-repeat
+            &.discount
+              bg-image("discount_4")
+            &.decrease
+              bg-image("decrease_4")
+            &.guarantee
+              bg-image("guarantee_4")
+            &.invoice
+              bg-image("invoice_4")
+            &.special
+              bg-image("special_4")
+          .text
+            line-height: 16px
+            font-size: 12px
+            color: rgb(7, 17, 27)
+    .pics
+      padding: 18px
+      .title
+        margin-bottom: 8px
+        line-height: 14px
+        color: rgb(7,17,27)
+        font-size: 14px
+      .pic-wrapper
+        width: 100%
+        overflow: hidden
+        white-space: nowrap
+        .pic-list
+          font-size: 0
+          .pic-item
+            display: inline-block
+            margin-right: 6px
+            width: 120px
+            height: 90px
+            &:last-child
+              margin: 0
 </style>
